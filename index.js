66607f27280b73c2718b7319f8dd2c8fade3f5cb
Abdulraheem Ahmad <abdulahmad586@gmail.com>
2023-06-10 22:17:22 +0100

    Added post route to /api/users

--- index.js
+++ index.js
@@ -1,15 +1,88 @@
 const express = require('express')
+const bodyParser = require('body-parser');
+require('dotenv').config()
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  }
+});
+
+let User = mongoose.model("User", userSchema);
+
+const exerciseSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  description: String,
+  duration: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: Date,
+    required:true,
+  }
+});
+
+let Exercise = mongoose.model("Exercise", exerciseSchema);
+
+const logSubSchema = new mongoose.Schema({
+  description: {
+    type: String,
+    required: true
+  },
+  duration: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  }
+});
+
+const logSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  count: {
+    type: Number,
+    required: true
+  },
+  log: {
+    type: [logSubSchema],
+    required: true
+  }
+});
+
+let Log = mongoose.model("Log", logSchema);
+
+
 const app = express()
 const cors = require('cors')
-require('dotenv').config()
 
+
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.post("/api/users/", async (req,res)=>{
 
+  const { username } = req.body;
+  const user= new User({username});
+  const result = await user.save();
+  res.json(result);
+})
 
 
 
