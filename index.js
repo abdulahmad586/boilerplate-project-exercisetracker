82c231896f221cde78ba24159fd6ac751ff8a6b3
Abdulraheem Ahmad <abdulahmad586@gmail.com>
2023-06-11 13:14:28 +0100

    Latest commit

--- index.js
+++ index.js
@@ -95,6 +95,7 @@ app.post("/api/users/", async (req,res)=>{
 app.post("/api/users/:_id/exercises", async (req,res)=>{
   const {_id} = req.params;
   const { description, duration, date=Date.now() } = req.body;
+  
   const user= await User.findOne({_id});
 
   const dateObj = new Date(date);
